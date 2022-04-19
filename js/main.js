aa0c41d6e880c4b17f881cbc35c079c7d525bfd1
Ritik Rajput <RIXUSINPUT696@gmail.com>
2022-04-19 06:03:12 +0530

    corrected email

--- js/main.js
+++ js/main.js
@@ -6,11 +6,8 @@ console.log(
 
 // hamburger show/hide
 hamBurger.addEventListener("click", () => {
-  console.log("mujhe mat chuo");
   showNav.classList.toggle("nav-active");
 });
-
-// const navClickAnimation = document.querySelectorAll(".nav__links a");
 
 // navClickAnimation.addEventListener("click", () => {
 //   console.log("hasthmaithoon");
